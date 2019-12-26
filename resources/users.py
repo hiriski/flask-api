85001d3ac3d322b27a3f09db02ae1ad829a81efc
riskiwebid <juna.karty@my.gavilan.edu>
2019-12-26 15:00:34 +0700

    Login successfull

--- resources/users.py
+++ resources/users.py
@@ -51,19 +51,57 @@ class UserList(Resource):
             user = models.User.create(
                 username = new_user_username,
                 # hash password usernya
-                password = md5(new_user_username.encode('utf-8')).hexdigest()
+                password = md5(new_user_password.encode('utf-8')).hexdigest()
                 )
             return marshal(user, user_fields) 
         else:
-            raise Exception('User ini sudah terdaftar bro')
+            return {'pesan':'Sorry, username ini sudah dipake orang!'}
 
 
 class User(Resource):
-    pass
+    # Fungsi ini akan otomatis terpanggil ketika BlogListnya dijalankan
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+
+        # untuk aturannya sendiri dan apa data yang mau kita terima kita simpan di .add_argument()
+        self.reqparse.add_argument(
+            'username',
+            required    = True,
+            help        = 'Username wajib ada', 
+            location    = ['form', 'json']
+        )
+        self.reqparse.add_argument(
+            'password',
+            required    = True,
+            help        = 'Password wajib ada', 
+            location    = ['form', 'json']
+        )
+        super().__init__() # pastiin dia manggil fungsi __init__ dari parentnya juga
+
+
+    def post(self):
+        kwargs = self.reqparse.parse_args()
+        # insert data user
+        user_username_registered = kwargs.get("username")
+        user_password_registered = kwargs.get("password")
+        # login
+        # uji apakah password dan usernamenya benar
+        try:
+            # buat variabel untuk password yang sudah di hash
+            hash_password = md5(user_password_registered.encode('utf-8')).hexdigest()
+            # uji metode sign in
+            user = models.User.get( (models.User.username == user_username_registered) & (models.User.password == hash_password) )
+ 
+        except models.User.DoesNotExist: 
+            return {'pesan':'User or password is wrong bro!'}
+
+        else:
+            return {'pesan':'Singin success!'}
+
 
 
 users_api    = Blueprint('resources.users', __name__)
 api          = Api(users_api)
 
 api.add_resource(UserList, '/user/register', endpoint="user/register")
-api.add_resource(User, '/user/login', endpoint="user/login")
+api.add_resource(User, '/user/signin', endpoint="user/signin")

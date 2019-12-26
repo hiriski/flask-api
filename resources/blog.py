d0c7f7e4a329237997665f727cc8c9ab83b4c5a8
riskiwebid <juna.karty@my.gavilan.edu>
2019-12-26 07:26:15 +0700

    Adding reqparse

--- resources/blog.py
+++ resources/blog.py
@@ -1,11 +1,58 @@
 #import jsonify karena kita akan me return json yang di ambil datanya dari database
 from flask import jsonify, Blueprint
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 
 import models # buatan sendiri
 
 # Buat sebuah class untuk nampilin semua blognya
 class BlogList(Resource):
+
+    # Fungsi ini akan otomatis terpanggil ketika BlogListnya dijalankan
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+
+        # untuk aturannya sendiri dan apa data yang mau kita terima kita simpan di .add_argument()
+        self.reqparse.add_argument(
+            'title',
+            required    = True, # field ini wajib, jika kosong user akan menerima pesan error
+            help        = 'Konten wajib ada', # pesan error
+            location    = ['form', 'json'] # kalau di postman x-www-form-urlendoded
+        )
+        
+        # ini hampir sama kek di atas
+        self.reqparse.add_argument(
+            'content',
+            required    = True, 
+            help        = 'Content blog harus ada bro',
+            location    = ['form', 'json']
+        )
+
+        # ini hampir sama kek di atas
+        self.reqparse.add_argument(
+            'create_at',
+            required    = True, 
+            help        = 'Tanggal harus ada bro',
+            location    = ['form', 'json']
+        )
+
+        super().__init__() # pastiin dia manggil fungsi __init__ dari parentnya juga
+    
+    # method post untuk nge-post data
+    def post(self):
+        args = self.reqparse.parse_args()
+
+        # insert data
+        # nanti jadinya akan seperti content='Ini judul blog' akan jadi key dan value
+        # itulah yang akan jadi parameter dari args
+        foo = models.Blog.create(**args)
+        return jsonify({
+                'sucess':'Data berhasil di POST',
+                'judul_blog':foo.title,
+                'konten_blog':foo.content,
+                'tanggal':foo.create_at
+            })
+
+
     def get(self):
 
         # siapin variable kosong dengan type data dictionary
@@ -35,7 +82,7 @@ class Blog(Resource):
             })
 
 
-blog_api   = Blueprint('resources.blog', __name__)
+blog_api    = Blueprint('resources.blog', __name__)
 api         = Api(blog_api)
 
 api.add_resource(BlogList, '/blog', endpoint="blogs")
